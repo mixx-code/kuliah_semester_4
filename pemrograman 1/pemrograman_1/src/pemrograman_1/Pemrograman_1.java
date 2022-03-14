856e949b64726920cf9718192bc99610ffb6c5dc
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-03-14 08:26:51 +0700

    belajar java

---
+++ pemrograman 1/pemrograman_1/src/pemrograman_1/Pemrograman_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pemrograman_1;
+
+/**
+ *
+ * @author rizki
+ */
+public class Pemrograman_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
