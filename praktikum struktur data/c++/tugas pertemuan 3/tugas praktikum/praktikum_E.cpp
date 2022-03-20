50164f45f0e27fd98c66a50bddb876a94c5a0d3c
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-03-20 15:59:48 -0700

    tugas

--- praktikum struktur data/c++/tugas pertemuan 3/tugas praktikum/praktikum_E.cpp
+++ praktikum struktur data/c++/tugas pertemuan 3/tugas praktikum/praktikum_E.cpp
@@ -5,5 +5,62 @@ struct nilai
 {
     char NIM[15];
     char nama[25];
-    int nilai_
+    int nilai_tugas;
+    int nilai_UTS;
+    int nilai_UAS;
+    int nilai_akhir;
+    char nilai_huruf;
 };
+
+int main()
+{
+int i;
+    int jumlah_indexs;
+    cout << "silahkan masukkan data pada form dibawah"<<endl;
+
+    cout<<endl;
+    cout << "BERAPA DATA YANG AKAN DI MASUKKAN \t: ";
+    cin >> jumlah_indexs;
+    cout<<endl;
+    cout<<endl;
+
+    nilai nl[jumlah_indexs];
+
+    for(i=0;i<jumlah_indexs;i++){
+
+    cout << "masukkan NIM\t: ";
+    gets(nl[i].NIM);
+    cin.getline(nl[i].NIM,15);
+    cout << "masukkan nama\t: ";
+    cin.getline(nl[i].nama,25);
+    cout << "masukkan nilai tugas\t:";
+    cin >> nl[i].nilai_tugas;
+    cout << "masukkan nilai UTS\t:";
+    cin >> nl[i].nilai_UTS;
+    cout << "masukkan nilai UAS\t:";
+    cin >> nl[i].nilai_UAS;
+
+    nl[i].nilai_akhir=(nl[i].nilai_tugas*0,2)+(nl[i].nilai_UTS*0.35)+(nl[i].nilai_UAS*0,45);
+
+    if(nl[i].nilai_akhir > 85 && nl[i].nilai_akhir <=100){
+        nl[i].nilai_huruf= 'A';
+    }else if(nl[i].nilai_akhir > 70 && nl[i].nilai_akhir <=85){
+        nl[i].nilai_huruf= 'B';
+    }else if(nl[i].nilai_akhir > 55 && nl[i].nilai_akhir <=70){
+        nl[i].nilai_huruf= 'C';
+    }else if(nl[i].nilai_akhir > 40 && nl[i].nilai_akhir <=55){
+        nl[i].nilai_huruf= 'D';
+    }else if(nl[i].nilai_akhir > 0 && nl[i].nilai_akhir <=40){
+        nl[i].nilai_huruf= 'E';
+    }
+    }
+
+
+    cout << "NO\tNIM\t\tNAMA\t\tNILAI TUGAS\tNILAI UTS\tNILAI UAS\tNILAI AKHIR\tGRADE"<<endl;
+    for(i=0;i<jumlah_indexs;i++){
+        cout << endl;
+        cout << i+1<<"\t"<<nl[i].NIM<<"\t"<<nl[i].nama<<"\t"<<nl[i].nilai_tugas<<"\t\t"<<nl[i].nilai_UTS<<"\t\t"<<nl[i].nilai_UAS<<"\t\t"<<nl[i].nilai_akhir<<"\t\t"<<nl[i].nilai_huruf<<endl;
+    }
+
+};
+
