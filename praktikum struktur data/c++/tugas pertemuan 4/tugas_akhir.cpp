8ed087b4cf1c913b8f4e53cced607e087671bfbc
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-03-25 20:00:18 +0700

    benerin tugas akhir pert4

--- praktikum struktur data/c++/tugas pertemuan 4/tugas_akhir.cpp
+++ praktikum struktur data/c++/tugas pertemuan 4/tugas_akhir.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 int main()
 {
-    int A[] = {1,2,3,4,5};
-    int i;
-    int *p = A;
-    for(i=0;i<5;i++){
-        cout << "Alamat memori : "<< &A[i]<<endl;
-        cout << "nilai : "<< *p+i<<endl;
+    int A[2][3] = {{1,2,3},{4,5,6}};
+    A[0];
+    A[1];
+    int (*p)[3]=A;
+
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+            cout <<"Nilai A "<<"["<<i<<"]"<<"["<<j<<"] = "<< A[i][j] << " dan alamatnya = " ;cout << &A[i][j] << "  " << endl;
+
+        }
     }
 }
 
