f3dac21b657427588fa775a5a7b1ee657f1861d0
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-04-04 10:30:34 +0700

    E

--- praktikum struktur data/c++/tugas pertemuan 5/tugas praktikum/praktikum_E.cpp
+++ praktikum struktur data/c++/tugas pertemuan 5/tugas praktikum/praktikum_E.cpp
@@ -1,8 +1,48 @@
-<<<<<<< HEAD
-
-=======
-#include <stdio.h>
-#include <iostream>
-#include <iomanip>
-#include <conio.h>
->>>>>>> 8889e478abdbd85482c1d6567cdbff3c2a9162e8
+    #include<stdio.h>
+    #include<iostream>
+    #include<iomanip>
+    #include<conio.h>
+    using namespace std;
+    const int N=100 ;
+    void Baca (int Nilai [ ], int &M) ;
+    void Cetak (int Nilai [ N ], int &M);
+    void Jumlah (int Nilai [ N ], int &M, int &Jlh, float &Rata) ;
+    main ()
+    {
+    int M;
+    int Nilai [N];
+    int Jlh =0;
+    float Rata;
+    cout<<"Banyak Elemen : ";
+    Baca (Nilai, M); //memanggil fungsi Baca
+    cout<<"\nElemen Elemen :";
+    Cetak(Nilai,M); //memanggil fungsi cetak
+    Jumlah (Nilai, M, Jlh, Rata) ; //memanggil fungsi Jumlah
+    cout<<"\Jumlah Bilangan : "<<Jlh;
+    cout<<"\nRata-Rata Bilangan :"<<Rata;
+    getch ( ) ;
+    }
+    void Cetak (int Nilai [], int &M)
+    {
+    int i;
+    for(i=0;i<M;i++)
+    {
+    cout<<setw (3) <<Nilai [ i ] ;
+    }
+    }
+    void Baca (int Nilai [], int & M)
+    {
+    int i ;
+    for (i=0;i<M;i++)
+    {
+    cout<<"Elemen ke-"<<i<<" : ";
+    cin>>Nilai [i];
+    }
+    }
+    void Jumlah (int Nilai [], int &M, int &Jlh, float &Rata)
+    {
+    int i;
+    for (i=0;i<M;i++)
+    Jlh+=Nilai [i] ;
+    Rata = (float) Jlh /M;
+    }
