f487503eea33976fd64fdbcc843667040fafdda7
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-04-04 11:23:34 +0700

    g

--- praktikum struktur data/c++/tugas pertemuan 5/tugas praktikum/praktikum_E.cpp
+++ praktikum struktur data/c++/tugas pertemuan 5/tugas praktikum/praktikum_E.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     #include<stdio.h>
     #include<iostream>
     #include<iomanip>
@@ -19,7 +18,7 @@
     cout<<"\nElemen Elemen :";
     Cetak(Nilai,M); //memanggil fungsi cetak
     Jumlah (Nilai, M, Jlh, Rata) ; //memanggil fungsi Jumlah
-    cout<<"\Jumlah Bilangan : "<<Jlh;
+    cout<<"\nJumlah Bilangan : "<<Jlh;
     cout<<"\nRata-Rata Bilangan :"<<Rata;
     getch ( ) ;
     }
@@ -47,53 +46,3 @@
     Jlh+=Nilai [i] ;
     Rata = (float) Jlh /M;
     }
-=======
-#include<stdio.h>
-#include<iostream>
-#include<iomanip>
-#include<conio.h>
-using namespace std;
-const int N=100 ;
-void Baca (int Nilai [ ], int &M) ;
-void Cetak (int Nilai [ N ], int &M);
-void Jumlah (int Nilai [ N ], int &M, int &Jlh, float &Rata) ;
-main ()
-{
- int M;
- int Nilai [N];
- int Jlh =0;
- float Rata;
- cout<<"Banyak Elemen : ";
-Baca (Nilai, M); //memanggil fungsi Baca
- cout<<"\nElemen Elemen :";
- Cetak(Nilai,M); //memanggil fungsi cetak
- Jumlah (Nilai, M, Jlh, Rata) ; //memanggil fungsi Jumlah
- cout<<"\Jumlah Bilangan : "<<Jlh;
- cout<<"\nRata-Rata Bilangan :"<<Rata;
- getch ( ) ;
-}
-void Cetak (int Nilai [], int &M)
-{
- int i;
- for(i=0;i<M;i++)
- {
- cout<<setw (3) <<Nilai [ i ] ;
- }
-}
-void Baca (int Nilai [], int & M)
-{
- int i ;
- for (i=0;i<M;i++)
- {
- cout<<"Elemen ke-"<<i<<" : ";
- cin>>Nilai [i];
- }
-}
-void Jumlah (int Nilai [], int &M, int &Jlh, float &Rata)
-{
- int i;
- for (i=0;i<M;i++)
- Jlh+=Nilai [i] ;
- Rata = (float) Jlh /M;
-}
->>>>>>> 1363e302aa2a766a1f20f3c659c1f4ac3fc5f6b7
--- praktikum struktur data/c++/tugas pertemuan 5/tugas praktikum/praktikum_G.cpp
+++ praktikum struktur data/c++/tugas pertemuan 5/tugas praktikum/praktikum_G.cpp
@@ -3,19 +3,19 @@
 #include<iostream>
 using namespace std;
 
-/*struct Mahasiswa
+struct Mahasiswa
 {
  char Nim [ 20 ] ;
  char Nama [ 25 ] ;
  char Alamat [ 40 ] ;
  short Umur ;
 };
-*/
-void mahasiswa(string *nim, string *nama, string *alamat);
+void Baca (Mahasiswa &Mhs);
+void Cetak (Mahasiswa &Mhs);
 main ( )
 {
-string nim,nama,alamat;
-mahasiswa(&nim,&nama,&alamat)
+ struct Mahasiswa Mhs;
+ struct Mahasiswa *ptr = &Mhs;
  cout<<"Membaca Nilai Anggota Struktur \n";
  Baca (Mhs);
  cout<<"\nMencetak Nilai Anggota Struktur ";
@@ -25,19 +25,19 @@ mahasiswa(&nim,&nama,&alamat)
 void Baca(Mahasiswa &Mhs)
 {
  cout<<"NIM : ";
- cin.getline(Mhs.Nim, 20) ;
+ cin.getline((*&Mhs).Nim, 20) ;
  cout<<"Nama :";
- cin.getline(Mhs.Nama,25) ;
+ cin.getline((*&Mhs).Nama,25) ;
  cout<<"Alamat :";
- cin.getline(Mhs.Alamat, 40);
+ cin.getline((*&Mhs).Alamat, 40);
  cout<<"Umur :";
- cin>>Mhs.Umur;
-}
-void Cetak (Mahasiswa &Mhs)
-{
- cout<<"\nNim : "<< Mhs.Nim;
- cout<<"\nNama :"<< Mhs.Nama;
- cout<<"\nAlamat :"<< Mhs.Alamat;
- cout<<"\nUmur :"<< Mhs.Umur;
+ cin>>(*&Mhs).Umur;
 }
 
+void Cetak (Mahasiswa &Mhs)
+{
+ cout<<"\nNim : "<< (*&Mhs).Nim;
+ cout<<"\nNama :"<< (*&Mhs).Nama;
+ cout<<"\nAlamat :"<< (*&Mhs).Alamat;
+ cout<<"\nUmur :"<< (*&Mhs).Umur;
+}
