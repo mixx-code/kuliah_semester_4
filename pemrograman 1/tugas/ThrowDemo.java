7cee1dfce4b1e50b3ad7faecb417c496c50f2360
Rizki febriansyah <rfebriansyah385@gmail.com>
2022-05-23 16:16:20 +0700

    praktikum pertemuan 9 struktur data

---
+++ pemrograman 1/tugas/ThrowDemo.java
@@ -0,0 +1,16 @@
+class ThrowDemo { 
+    public static void main(String args[]){ 
+    String input = "invalid input"; 
+    try { 
+    if (input.equals("invalid input")) { 
+    throw new RuntimeException("throw demo"); 
+    } else { 
+    System.out.println(input); 
+    } 
+    System.out.println("After throwing"); 
+    } catch (RuntimeException e) { 
+    System.out.println("Exception caught here."); 
+    System.out.println(e); 
+    } 
+    } 
+}
